a3f24c66f6c1d83560a9eb60629ece5d158f5ff4
WorriorBeast <128549074+WorriorBeast@users.noreply.github.com>
2023-10-10 19:40:48 -0700

    Connect javascript to number pad to display number

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
-const firstNumber = null;
-const operator = null;
-const secondNumber = null;
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let displayValue = null;
+
+const buttons = document.querySelectorAll('button');
 
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
@@ -18,7 +21,7 @@ function divide(firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 }
 
-function operate(operator) {
+function operate(operator, firstNumber, firstNumber) {
     if (operator === '+') {
         add(firstNumber, secondNumber);
 
@@ -32,3 +35,20 @@ function operate(operator) {
         divide(firstNumber, secondNumber);
     }
 }
+
+function clickButton() {
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            if (this.classList.contains('number')) {
+                updateDisplay(this.value);
+            }
+        })
+    }
+}
+clickButton();
+
+function updateDisplay(displayValue) {
+    const display = document.querySelector('.display');
+    display.textContent = displayValue;
+}
+updateDisplay(0);
